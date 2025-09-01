552bad32b276ab0f589e2e276e0b643152c1552d
screengui <134831841+screengui@users.noreply.github.com>
2025-09-01 22:11:36 +0800

    Create longestanswerwin.lua

---
+++ PiHub/longestanswerwin.lua
@@ -0,0 +1,36 @@
+local Library = loadstring(game:HttpGet("https://pi-hub.pages.dev/lib/ui.lua"))()
+
+enable = false
+function answer()
+    while enable == true do 
+        if game:GetService("ReplicatedStorage").Time.TimeLeft.Value < 3 then
+            local args = {
+                [1] = game:GetService("ReplicatedStorage").HintAnswer.Value,
+                [2] = 4
+            }
+            
+            game:GetService("ReplicatedStorage").SubmittedAnswer:FireServer(unpack(args))
+        end
+    end
+end
+
+local Main = Library:Create("Pi Hub - Longest Answer Wins")
+local TabH = Main.MakeTab("Pi Hub - Longest Answer Wins (Home)", 18899804355)
+
+local Sections = {
+    ['Home'] = {
+        Main = TabH.MakeSection("Main"), -- Tab.MakeSection(<string: Section_Name>)
+        Game = TabH.MakeSection("Game")
+    }
+}
+
+local main = Sections.Home.Main
+main.Label("Welcome To Pi Hub!")
+main.Label("Join Our Discord: https://discord.gg/pihub")
+
+local game = Sections.Home.Game
+
+game.Toggle("Auto Farm", enable, function(Bool)
+    enable = Bool
+    answer()
+end)
