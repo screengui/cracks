9f27ca535d4fd81f52175a4e2b01c031c8db9023
screengui <134831841+screengui@users.noreply.github.com>
2026-01-31 20:47:50 +0800

    Create LinniFreeLoader-deobfuscated.lua

---
+++ LinniFreeLoader-deobfuscated.lua
@@ -0,0 +1,6 @@
+--Cracked by Purple Apple / screengui
+local Players = game:GetService('Players')
+local v3 = game.Players.LocalPlayer.IsInGroup(game.Players.LocalPlayer, 36012386)
+local MarketplaceService = game:GetService('MarketplaceService')
+local v5 = MarketplaceService.GetProductInfo(MarketplaceService, 131623223084840)
+warn("未匹配到任何脚本url支持此游戏")
